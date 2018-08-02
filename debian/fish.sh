ac07875529aadec16619549725658838b7879598
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2018-08-02 22:06:45 +0900

    Fix config for fish

---
+++ debian/fish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-add-repository -y ppa:fish-shell/release-2
+sudo apt update
+sudo apt install -y fish
--- install/fisher.sh
+++ install/fisher.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+. ../util.sh
 
 if !(fish -c "type fisher > /dev/null 2>&1"); then
     curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 fi
 
+packages=(`lines fisher | xargs`)
+
+for package in "${packages[@]}"
+do
+    fish -c "fisher ${package}"
+done
