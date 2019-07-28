c854008efba888a4d274fa9269460e29e75bc195
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2019-07-29 06:22:14 +0900

    Modify font.sh

--- font.sh
+++ font.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+. ./util.sh
+
+# OS毎に必要なソフトウェアのインストール
 case "$OSTYPE" in
     darwin*)
         brew install unzip fontconfig
@@ -7,8 +12,13 @@ esac
 
 TMP_DIR=/var/tmp
 
-curl https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip -o "${TMP_DIR}/Cica.zip"
+info "Download Cica to ${TMP_DIR}/Cica.zip"
 
+curl -L https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip -o "${TMP_DIR}/Cica.zip" --progress-bar
+
+info "Complete to download Cica.zip"
+
+# OS毎にフォントのインストール先を指定
 case "$OSTYPE" in
     darwin*)
         INSTALL_DIR=~/Library/Fonts/Cica ;;
@@ -16,5 +26,24 @@ case "$OSTYPE" in
         INSTALL_DIR=~/.fonts/Cica ;;
 esac
 
-unzip -u "${TMP_DIR}/Cica.zip" -d "$INSTALL_DIR"
-sudo fc-cache -fv
+info "unzip ${TMP_DIR}/Cica.zip to ${INSTALL_DIR}"
+
+# フォントの入った圧縮ファイルを解凍
+unzip -quo "${TMP_DIR}/Cica.zip" -d "${INSTALL_DIR}"
+
+info "Complete to unzip Cica.zip"
+
+info "Updating font cache"
+
+password
+
+result=0
+output=$(echo "$password" | sudo -S fc-cache -fv 2>&1 > /dev/null) &
+progress_indicator "Updating font cache"
+
+if [ "$result" = "0" ]; then
+    info "Complete to update font cache"
+else
+    error "Fail to update font cache"
+    echo "$output"
+fi
--- util.sh
+++ util.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-info () {
+info() {
     echo -e "\e[32m[info]\e[m $1"
 }
 
-debug () {
-    echo -e "\e[34m[info]\e[m $1"
+progress() {
+    echo -en "\r\e[32m[progress]\e[m $1"
+}
+
+debug() {
+    echo -e "\e[34m[debug]\e[m $1"
 }
 
 error () {
-    echo -e "\e[31m[info]\e[m $1"
+    echo -e "\e[31m[error]\e[m $1"
 }
 
-warn () {
-    echo -e "\e[33m[info]\e[m $1"
+warn() {
+    echo -e "\e[33m[warn]\e[m $1"
 }
 
-lines () {
+lines() {
     grep -v -e '^\s*#' -e '^\s*$' "$1"
 }
 
@@ -30,4 +34,30 @@ sfrm() {
 	else
 		rm -rf $1
 	fi
+}
+
+password() {
+    if ! ${password+:} false
+    then
+        printf "password: "
+        read -s password
+    fi
+}
+
+progress_indicator() {
+    pid=$! # Process Id of the previous running command
+
+    spin='-\|/'
+
+    i=0
+    while kill -0 $pid 2>/dev/null
+    do
+        i=$(( (i+1) %4 ))
+        progress "$1 ${spin:$i:1}"
+        sleep .1
+    done
+
+    echo ""
+    wait "$pid"
+    result=$?
 }
